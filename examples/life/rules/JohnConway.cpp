239e8605158833da713087d327439e684d9c6f42
JesseThomsen <jesse.thomsen@mymail.champlain.edu>
2024-09-12 11:19:58 -0400

    Added JohnConway::Step Added JohnConway::CountNeighbors

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,36 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  for(int i = - world.SideSize()/2; i <= world.SideSize()/2; i++) {
+    for(int j = - world.SideSize()/2; j <= world.SideSize()/2; j++) {
+
+      bool isAlive = world.Get({i,j});
+      int neighbours = CountNeighbors(world, {i,j});
+
+      if(isAlive) {
+        if(neighbours < 2) {
+        world.SetNext({i,j}, false);
+        }
+      }
+      else {
+
+      }
+    }
+  }
+
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+
+  int count = 0;
+
+  for(int x=-1; x<=1; x++) {
+    for(int y=-1; y<=1;y++) {
+      Point2D p = point + Point2D(x, y);
+      count += world.Get(p);
+    }
+  }
+
+  return count;
 }
--- examples/life/rules/JohnConway.h
+++ examples/life/rules/JohnConway.h
@@ -12,4 +12,31 @@ public:
   GameOfLifeTileSetEnum GetTileSet() override { return GameOfLifeTileSetEnum::Square; };
 };
 
+/*
+*#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+int x, y, steps;
+cin >> x >> y >> steps;
+
+string line;
+getline(cin, line); // ignore next line
+
+char* state; // matrix linearization;
+char* nextState;
+state = new char[y*x];
+nextState = new char[y*x];
+
+for(int j=0; j<y; j++) {
+  getline(cin, line);
+  for(int i=0; i<x;i++)
+    state[j*x + i] = line[i];
+}
+
+}
+ *
+ */
 #endif  // MOBAGEN_JOHNCONWAY_H
