56fb69f413f9616f4cd9e6fd504d80beb88f38ed
JesseThomsen <jesse.thomsen@mymail.champlain.edu>
2024-09-19 11:17:17 -0400

    Added JohnConway::Step to now have statements for if neighbors = 2, 3, or <3.
    Changed JohnConway::CountNeighbors to not count itself

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -1,5 +1,8 @@
 #include "JohnConway.h"
 
+#include <iostream>
+#include <ostream>
+
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
@@ -9,13 +12,26 @@ void JohnConway::Step(World& world) {
       bool isAlive = world.Get({i,j});
       int neighbours = CountNeighbors(world, {i,j});
 
+
+      std::cout << "point (" << i << ", " << j << ") has [" << neighbours << "] neighbors" << std::endl;
+
       if(isAlive) {
         if(neighbours < 2) {
         world.SetNext({i,j}, false);
         }
+
+        else if(neighbours == 2 || neighbours == 3) {
+          world.SetNext({i,j}, true);
+        }
+
+        else if(neighbours > 3) {
+          world.SetNext({i,j}, false);
+        }
       }
       else {
-
+        if(neighbours == 3) {
+          world.SetNext({i,j}, true);
+        }
       }
     }
   }
@@ -30,9 +46,12 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   for(int x=-1; x<=1; x++) {
     for(int y=-1; y<=1;y++) {
       Point2D p = point + Point2D(x, y);
-      count += world.Get(p);
+
+      // check if point is itself
+      if(p != point) {
+      count += static_cast<int>(world.Get(p));
+      }
     }
   }
-
   return count;
 }
